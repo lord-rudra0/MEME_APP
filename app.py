e51716a63cc303b50670ba2bb06edad3adc092a1
rudra pratap singh <rudrapratapsingh2026@cs.ajce.in>
2024-11-20 13:19:37 +0530

    wat

--- app.py
+++ app.py
@@ -14,15 +14,10 @@ def meme():
     meme_large = response["preview"][-2]
     subreddit = response["subreddit"]
     return meme_large, subreddit
-
-
-
 @app.route('/')
 def index():
     meme_pic, subreddit = meme()
     return render_template('index.html', meme_pic=meme_pic, subreddit=subreddit)
-
-
 if __name__ == '__main__':
     app.run(debug=True)
 
